26c3245b5cc032dcaa318d124aa672b620384ee4
Asmer Chougle <asmeraltafchougle@Asmers-MacBook-Air.local>
2022-12-14 22:52:50 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import onSocket from "./socket.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const httpServer = createServer(app);
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/public/index.html");
+	console.log('connedted')
+});
+
+app.use(express.static(__dirname + "/public"));
+
+const io = new Server(httpServer);
+onSocket(io);
+
+const port = process.env.PORT || '1234';
+httpServer.listen(port, () => console.log(`Listening on port ${port}...`));
